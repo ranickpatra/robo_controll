f00f06fb9c1417f22e070c7d7e7940f0ae4edebc
ranickpatra <ranickpatra545@gmail.com>
2018-11-07 15:11:35 +0530

    fds

--- clean_print.py
+++ clean_print.py
@@ -1,11 +1,11 @@
 import sys
-length = 0
+length = 50
 
 def Cprint(data):
     global length
     l = sys.stdout.write(str(data))
-    if l > length:
-        lenght = l
+    #if l > length:
+    #    lenght = l
     sys.stdout.write(" "*(length-l))
     sys.stdout.flush()
     sys.stdout.write("\b"*length)
