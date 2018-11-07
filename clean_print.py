270624c292a9b30844248bbe892610d4df25be74
ranickpatra <ranickpatra545@gmail.com>
2018-11-07 14:54:39 +0530

    fds

--- clean_print.py
+++ clean_print.py
@@ -2,6 +2,6 @@ import sys
 
 def Cprint(data, lenght=80):
     l = sys.stdout.write(str(data))
-    sys.stdout.write(" "*(length-l))
+    sys.stdout.write(" "*(lenght-l))
     sys.stdout.flush()
     sys.stdout.write("\b"*lenght)
