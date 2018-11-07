64921303de8b587fea44bb6358476a9471adf0b1
ranickpatra <ranickpatra545@gmail.com>
2018-11-07 14:21:46 +0530

    dkj

---
+++ postion.py
@@ -0,0 +1,30 @@
+from smbus2 import SMBusWrapper
+import time
+import sys
+
+address = 0x05
+
+def readData():
+    with SMBusWrapper(1) as bus:
+        try:
+            block = bus.read_i2c_block_data(address, 0, 8)
+        except:
+            print("I2C ERROR while reading")
+            return
+
+        for b in block:
+            if b & 0xFF == 0xFF:
+                return
+        data = []
+        data.append(block[0] << 8 | block[1])
+        data.append(block[2] << 8 | block[3])
+        data.append(block[4] << 8 | block[5])
+        data.append(block[6] << 8 | block[7])
+        print(data, end="")
+        sys.stdout.flush()
+
+
+
+while True:
+    readData()
+    time.sleep(0.1)
