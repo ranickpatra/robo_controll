7fe3bca56a5ff193bd28d9e038eddcab960c93f1
ranickpatra <ranickpatra545@gmail.com>
2018-11-07 14:56:58 +0530

    ad

--- clean_print.py
+++ clean_print.py
@@ -1,7 +1,11 @@
 import sys
+lenght = 0
 
-def Cprint(data, lenght=80):
+def Cprint(data):
+    global x
     l = sys.stdout.write(str(data))
+    if l > lenght:
+        lenght = l
     sys.stdout.write(" "*(lenght-l))
     sys.stdout.flush()
     sys.stdout.write("\b"*lenght)
