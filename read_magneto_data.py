69b9a8822ae2a1d5dca6ec14ec91b8156646fe67
ranickpatra <ranickpatra545@gmail.com>
2019-01-20 12:10:27 +0530

    fggf

--- read_magneto_data.py
+++ read_magneto_data.py
@@ -4,7 +4,7 @@ import numpy as np
 import queue
 
 address = 0x0D
-max_queue_size = 8
+max_queue_size = 20
 
 x_queue = queue.Queue(maxsize=max_queue_size)
 y_queue = queue.Queue(maxsize=max_queue_size)
