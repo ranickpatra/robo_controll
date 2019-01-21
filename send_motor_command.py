ca61567c598a5601778b3b7199c8d4e17460c6b0
ranickpatra <ranickpatra.nalikul@gmail.com>
2019-01-21 13:36:19 +0530

    sdfds

--- send_motor_command.py
+++ send_motor_command.py
@@ -5,18 +5,18 @@ address = 0x06
 
 
 while True:
+    data = [0, 100]
     try :
-        data = int(input("Enter 0, 1, 2, 3, 4 :: "))
+        data[0] = int(input("Enter 0, 1, 2, 3, 4 :: "))
     except KeyboardInterrupt:
         print("\nexit")
         sys.exit()
     except :
-        data = 0
+        data[0] = 0
 
-    if data > 4 :
-        data = 0
-    #data = data << 8 | 200;
+    if data[0] > 4 :
+        data[0] = 0
+
     with SMBusWrapper(1) as bus:
-        bus.write_byte(address, data)
-        bus.write_byte(address, 150)
-		#ok
+        for d in data:
+            bus.write_byte(address, d)
