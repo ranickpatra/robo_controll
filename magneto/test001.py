21c1cee28e340dff5752138468d5b2a2044eeeb2
ranickpatra <ranickpatra.nalikul@gmail.com>
2019-01-28 17:33:49 +0530

    adsad

--- magneto/test001.py
+++ magneto/test001.py
@@ -6,6 +6,6 @@ mag_sens = qmc5883l.QMC5883L()
 
 while True:
     [x, y, z] = mag_sens.get_magnet()
-    direc = math.atan(y/x) * 180 / math.pi
+    direc = math.atan(y) * 180 / math.pi
     print("{:.4f}, {:.4f}, {:.4f} {:.2f}".format(x, y, z, direc))
     time.sleep(0.2)
