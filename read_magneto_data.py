cb8b5d686b7c52392edeb17c67a56ecb9798ea94
ranickpatra <ranickpatra.nalikul@gmail.com>
2019-02-09 06:57:57 +0530

    sfgfsdg

--- read_magneto_data.py
+++ read_magneto_data.py
@@ -1,9 +1,10 @@
 import py_qmc5883l
 import time
+import math
 
 sensor = py_qmc5883l.QMC5883L()
 
 while True:
     [x, y, z] = sensor.get_magnet()
-    print(x, y, z)
+    print(math.degrees(math.atan2(y, x)))
     time.sleep(0.1)
