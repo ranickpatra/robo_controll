d4155ad48d265bb7e9c4ff6dccb6b17fb2004118
ranickpatra <ranickpatra545@gmail.com>
2019-02-09 20:15:24 +0530

    xfg

--- postion.py
+++ postion.py
@@ -83,9 +83,13 @@ if __name__ == '__main__':
 
         data = [d * 0.0436 for d in data]
 
-        print("%.0f, %.0f, %.0f, %.0f"  % (data[d_format['fwd']], data[d_format['bkwd']], data[d_format['left']], data[d_format['right']]))
+        #print("%.0f, %.0f, %.0f, %.0f"  % (data[d_format['fwd']], data[d_format['bkwd']], data[d_format['left']], data[d_format['right']]))
         # if magnet_data:
         #     print(magnet_data)
+        pos['x'] = int(data[d_format['bkwd']] / 20)
+        pos['y'] = int(data[d_format['left']] / 20)
+        pos_matrix.fill(0)
+        pos_matrix[pos['x'], pos['y']] = 1
 
 
         time.sleep(0.1)
