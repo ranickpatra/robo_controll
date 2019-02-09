5bfe188d4ef603ff672ba9e4410b2456a82737e2
ranickpatra <ranickpatra.nalikul@gmail.com>
2019-02-09 10:29:04 +0530

    fgdfg

--- read_magneto_data.py
+++ read_magneto_data.py
@@ -1,11 +1,24 @@
-import py_qmc5883l
+from smbus2 import SMBusWrapper
 import time
-import math
 
-sensor = py_qmc5883l.QMC5883L()
 
-while True:
-    [x, y, z] = sensor.get_magnet()
-    deg = math.degrees(math.atan2(y, x))
-    print(x, y, z)
+def readData():
+    with SMBusWrapper(1) as bus:
+        try:
+            block = bus.read_i2c_block_data(address, 0, 6)
+        except:
+            print("I2C ERROR")
+            return
+
+        for b in block:
+            if b & 0xFF == 0xFF:
+                return
+        data = []
+        data.append(block[0] << 8 | block[1])
+        data.append(block[2] << 8 | block[3])
+        data.append(block[4] << 8 | block[5])
+        print(data)
+
+if __name__ == '__main__':
+    readData()
     time.sleep(0.2)
