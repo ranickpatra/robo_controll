305b6953771d50d8cf3859e59410fbb66509909f
Ranick Patra <ranickpatra.nalikul@gmail.com>
2019-03-06 22:33:55 +0530

    Update send_motor_command_01.py

--- send_motor_command_01.py
+++ send_motor_command_01.py
@@ -1,5 +1,6 @@
 from smbus2 import SMBusWrapper
 import sys
+import msvcrt
 
 address = 0x06
 
@@ -7,7 +8,16 @@ address = 0x06
 while True:
     data = [0, 200]
     try :
-        data[0] = int(input("Enter 0, 1, 2, 3, 4 :: "))
+        #data[0] = int(input("Enter 0, 1, 2, 3, 4 :: "))
+		aa = msvcrt.getch()
+		if aa == b'w':
+			data[0] = 1
+		elif aa == b's':
+			data[0] = 2
+		elif aa == b'd':
+			data[0] = 3
+		elif aa == b'a':
+			data[0] = 4
     except KeyboardInterrupt:
         print("\nexit")
         sys.exit()
@@ -16,8 +26,11 @@ while True:
 
     if data[0] > 4 :
         data[0] = 0
-    print(data)
-    with SMBusWrapper(1) as bus:
-        bus.write_i2c_block_data(address, 0, data)
-        #block = bus.read_i2c_block_data(address, 0, 2)
-        #print(block)
+    #print(data)
+	try:
+		with SMBusWrapper(1) as bus:
+			bus.write_i2c_block_data(address, 0, data)
+			#block = bus.read_i2c_block_data(address, 0, 2)
+			#print(block)
+	except:
+		pass
